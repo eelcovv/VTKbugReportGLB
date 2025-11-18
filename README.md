5aac635e91fec4d7398ba980c5020f5d5f3b136a
Eelco van Vliet <eelco@davelab.com>
2025-11-18 10:33:47 +0100

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Import GLB in VTK
+
+This project demnostrates an example of a erouneous import of glb into vtk
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from vtkbugreportglb!")
+
+
+if __name__ == "__main__":
+    main()
